36531bbb2e332bafe84944d1fb9a15aa1e7c5138
Ken Wilcox <wilcoxk@gmail.com>
2015-06-25 21:19:06 -0600

    fetching Minions

--- DependencyInjectionExample/MinionService.swift
+++ DependencyInjectionExample/MinionService.swift
@@ -19,6 +19,12 @@ class MinionService {
     println("pretend we're getting minions asynchronously")
     let minionData = [Minion(name: "Bob"), Minion(name: "Dave")]
     completionHandler(MinionDataResult.Success(minionData))
+    
+    // Uncomment if you want to test our an error scenario
+    let error = NSError(domain: "Error",
+      code: 400,
+      userInfo: [NSLocalizedDescriptionKey : "Oops! The Minions are missing on a new fun adventure!"])
+    completionHandler(MinionDataResult.Failure(error))
   }
   
 }
--- DependencyInjectionExample/ViewController.swift
+++ DependencyInjectionExample/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+  
+  var dataSource: [Minion]?
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    fetchMinions()
   }
   
   override func didReceiveMemoryWarning() {
@@ -20,6 +23,35 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
   
-  
+  func fetchMinions(minionService: MinionService = MinionService()) {
+    minionService.getTheMinions { (minionDataResult) -> Void in
+      switch(minionDataResult) {
+      case .Success(let minionsData):
+        self.dataSource = minionsData
+        self.tableView.reloadData()
+      case .Failure(let error):
+        let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "Oh Snap!", style: .Cancel, handler: nil)
+        alertController.addAction(okAction)
+        self.presentViewController(alertController, animated: true, completion: nil)
+      }
+    }
+  }
 }
 
+// MARK: UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return dataSource?.count ?? 0
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("minionCell") as! UITableViewCell
+    
+    if let minion = dataSource?[indexPath.row] {
+      cell.textLabel?.text = minion.name
+      cell.imageView?.image = UIImage(named: minion.name)
+    }
+    return cell
+  }
+}
